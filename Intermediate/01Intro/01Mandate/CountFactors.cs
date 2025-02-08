5d264674554ba2c98bff184b212100b8b058bce9
Arunprasad A <aa00938143@techmahindra.com>
2025-02-09 00:12:59 +0530

    Find the factors of a number.

---
+++ Intermediate/01Intro/01Mandate/CountFactors.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intermediate.CountFactors
+{
+    public static class Q1CountFactors
+    {
+        public static void CountFactors2()
+        {
+            Console.WriteLine("Count Factors 2");
+            Console.WriteLine("---------------");
+            
+            Console.Write("Enter a number to find its factors : ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            
+            int result = CountFactors(input);
+            Console.Write("Number of factors of {0} is: {1}", input, result);
+        }
+
+        private static int CountFactors(int A) 
+        {
+            int count = 0;
+
+            for(int i=1; i * i <= A; i++)
+            {
+                if( A % i == 0)
+                {
+                    if(i == A / i)
+                    {
+                        count = count + 1;
+                    } 
+                    else
+                    {
+                        count = count + 2; 
+                    }
+                }
+            }
+            return count;
+        }
+    }
+}
